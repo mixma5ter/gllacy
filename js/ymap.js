f60dfd0936ccab5533f8521061ca65473a2fb127
mixma5ter <deejay999@yandex.ru>
2018-06-11 15:57:25 +0300

    правки правки правки

--- js/ymap.js
+++ js/ymap.js
@@ -8,11 +8,24 @@ function init(){
   });
 
   var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+    // Опции.
+    // Необходимо указать данный тип макета.
     iconLayout: 'default#image',
+    // Своё изображение иконки метки.
     iconImageHref: 'img/map_pin.png',
+    // Размеры метки.
     iconImageSize: [218, 142],
+    // Смещение левого верхнего угла иконки относительно
+    // её "ножки" (точки привязки).
     iconImageOffset: [-40, -140]
   });
 
   myMap.geoObjects.add(myPlacemark);
+  myMap.controls.remove("rulerControl");
+  myMap.controls.remove("searchControl");
+  myMap.controls.remove("trafficControl");
+  myMap.controls.remove("typeSelector");
+  myMap.controls.remove("zoomControl");
+  myMap.controls.remove("geolocationControl");
+  myMap.controls.remove("routeEditor");
 }
